45543295562e37f4466ba30a98953729c137b01a
jollysingla2000 <54760435+jollysingla2000@users.noreply.github.com>
2023-06-15 18:20:10 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,23 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
+function isAnagram(str1,str2) {
+  str1 = str1.toLowerCase()
+  str2 = str2.toLowerCase()
+  let len1 = str1.length;
+  let len2 = str2.length;
+  if (len1!=len2){
+    return false;
+  }
+  let a = str1.split('').sort().join('');
 
+  let b = str2.split('').sort().join('');
+  //console.log(a,b);
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isAnagram;
